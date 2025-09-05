1a04355e4556cd72356404cdd98628d505d7fab6
kamruj-akash <source.akash@gmail.com>
2025-09-06 00:10:01 +0600

    Added till Level section:

--- script/index.js
+++ script/index.js
@@ -15,15 +15,16 @@ const allLessons = (lessons) => {
   lessons.forEach((lesson) => {
     const newDiv = document.createElement("div");
     newDiv.innerHTML = `
-    <button onClick="loadLessonLevel(${lesson.level_no})" class="btn bg-transparent border-2 border-[#422AD5] text-[#422AD5]" ><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no} </button>
+    <button  onClick="loadLessonLevel(${lesson.level_no})" class="btn bg-transparent border-2 border-[#422AD5] text-[#422AD5]" ><i class="fa-solid fa-book-open"></i> Lesson ${lesson.level_no} </button>
     `;
     levelContainer.append(newDiv);
   });
 };
 
 const loadLessonLevel = (id) => {
-  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  console.log(id);
 
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => lessonWords(data.data));
@@ -32,6 +33,7 @@ const loadLessonLevel = (id) => {
 const lessonWords = (words) => {
   const wordsCards = getID("words_cards");
   wordsCards.innerHTML = "";
+
   words.forEach((words) => {
     const word = words.word;
     const meaning = words.meaning;
@@ -42,7 +44,7 @@ const lessonWords = (words) => {
     <div class="card text-black bg-white rounded-lg">
           <div class="card-body items-center text-center">
             <h2 class="card-title text-4xl">${word}</h2>
-            <p class="my-4 text-xl">Meaning /Pronounciation</p>
+            <p class="my-4 text-xl">Meaning / Pronounciation</p>
             <h2 class="card-title text-4xl text-bangla">"${meaning} / ${pronunciation}"</h2>
             <div class="justify-between w-full">
               <button class="btn btn-ghost">
